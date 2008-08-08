d793ae29ba75ad58eba868ac97937ed182dfca8a
netcarver <nc.github@netcarving.com>
2008-08-08 13:56:53 +0800

    Remove template comments

--- rss_admin_db_manager.php
+++ rss_admin_db_manager.php
@@ -1,21 +1,11 @@
 <?php
-// This is a PLUGIN TEMPLATE.
-// Copy this file to a new name like abc_myplugin.php.  Edit the code, then
-// run this file at the command line to produce a plugin for distribution:
-// $ php abc_myplugin.php > abc_myplugin-0.1.txt
 
-// Plugin name is optional.  If unset, it will be extracted from the current
-// file name. Uncomment and edit this line to override:
 $plugin['name'] = 'rss_admin_db_manager';
 $plugin['version'] = '4.3';
 $plugin['author'] = 'Rob Sable';
 $plugin['author_uri'] = 'http://www.wilshireone.com/';
 $plugin['description'] = 'Database management system.';
 
-// Plugin types:
-// 0 = regular plugin; loaded on the public web side only
-// 1 = admin plugin; loaded on both the public and admin side
-// 2 = library; loaded only when include_plugin() or require_plugin() is called
 $plugin['type'] = '1';
 
 @include_once('zem_tpl.php');
